d2736d9eb958020ea9680053b5144a5c503079e8
Ishtiaq Mahmud Fahim <imfahim28@isrt.ac.bd>
2025-03-15 12:48:12 +0600

    Add files via upload

---
+++ 01 Python Basics I/03 Math Functions.py
@@ -0,0 +1,9 @@
+# Math Functions
+
+print(round(3.1))
+print(abs(-20))
+
+# For more, see the Python documentation: https://docs.python.org/3/library/math.html
+
+print(bin(5))
+print(int('0b101', 2)) # Convert binary to integer
